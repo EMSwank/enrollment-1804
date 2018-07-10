8d7590dcb0ad973e012384eea91eca8df607b817
Eliot Swank <eliotswank@gmail.com>
2018-07-10 15:48:17 -0600

    attempting to recover from db rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710213506) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "description"
-    t.string "street"
-    t.string "city"
-    t.integer "zip_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "student_id"
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
