a68797d08126f3a49e4c924ec7521303cb7ed067
Eliot Swank <eliotswank@gmail.com>
2018-07-10 13:42:47 -0600

    adds student controller and routes

--- spec/features/students/users_see_an_individual_student_spec.rb
+++ spec/features/students/users_see_an_individual_student_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 describe 'user visits student show page' do
   it "sees an individual student" do
-    student1 = Student.create(name: "David")
-    student2 = Student.create(name: "Slash")
+    student = Student.create(name: "David")
 
     visit student_path(student)
 
-    expect(page).to have_content(student1.name)
-    expect(page).to have_content(student2.name)
+    expect(page).to have_content(student.name)
   end
 end
