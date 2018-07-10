d2387a1a3b90c247de6983537e06dd531b867e7e
Eliot Swank <eliotswank@gmail.com>
2018-07-10 15:07:29 -0600

    adds test for linking students from index to show page

--- spec/features/students/user_can_see_a_list_of_students_spec.rb
+++ spec/features/students/user_can_see_a_list_of_students_spec.rb
@@ -13,6 +13,19 @@ describe "user" do
       expect(page).to have_content(student2.name)
       expect(page).to have_content(student3.name)
     end
+    context "clicks on student name" do
+      it "sees a list of student names" do
+        student1 = Student.create(name: "Larry")
+        student2 = Student.create(name: "Moe")
+        student3 = Student.create(name: "Curly")
+
+        visit students_path
+        click_on student1.name
+
+        expect(current_path).to eq(student_path(student1))
+        expect(page).to have_content(student1.name)
+      end
+    end
   end
 end
 
@@ -21,4 +34,12 @@ end
 As a user
 When I visit `/students`
 I see a list of student names
+
+
+As a user
+When I visit `/students`
+And I click on a student's name
+I am taken to a show page for that student
+
 =end
+
