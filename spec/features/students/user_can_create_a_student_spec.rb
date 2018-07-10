455c236c0b2b022c64d0217a76dc7895fda5b90a
Eliot Swank <eliotswank@gmail.com>
2018-07-10 13:58:42 -0600

    adds test for student creation

---
+++ spec/features/students/user_can_create_a_student_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "user visits /students/new" do
+  context "fills out a form to create a student" do
+    it "creates a student and goes to the student show page" do
+      visit new_student_path
+
+      fill_in :name, with:  "Axl Rose"
+      click_on "Create Student"
+
+      expect(current_path).to eq(new_student_path)
+      expect(page).to have_content(name)
+    end
+  end
+end
+
+=begin
+As a user
+When I visit `/students/new`
+And I fill in name
+And I click submit
+I am on the student show page
+And I see that student's name
+=end
